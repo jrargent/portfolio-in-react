9eb054fc2f193ebd46ed6511f538d5ee4beee5fa
Argent <jrargent42@gmail.com>
2023-02-06 22:12:13 -0500

    initial set up and beginning of component creation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <About></About>
+
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+function About() {
+  return (
+    <div>
+      <h1>This is about me</h1>
+      <Button variant="info">I am a button!</Button>
+    </div>
+  );
+}
+
+export default About;
